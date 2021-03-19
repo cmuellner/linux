c64a2dece1f96814c28306df7150ae40429164db
Guo Ren <guoren@linux.alibaba.com>
2021-03-19 17:53:32 +0100

    riscv: Replace test-and-set locks by qlocks
    RISC-V uses a test-and-set algorithm to implement spinlocks and rwlocks.
    However, they don't provide any ordering gurantees.
    
    Let's address this by removing the test-and-set lock implementation
    and switch over to the generic MCS lock implementation of the kernel.
    
    Signed-off-by: Guo Ren <guoren@linux.alibaba.com>
    Signed-off-by: Christoph Muellner <cmuellner@linux.com>

--- arch/riscv/include/asm/spinlock_types.h
+++ arch/riscv/include/asm/spinlock_types.h
@@ -6,20 +6,11 @@
 #ifndef _ASM_RISCV_SPINLOCK_TYPES_H
 #define _ASM_RISCV_SPINLOCK_TYPES_H
 
-#ifndef __LINUX_SPINLOCK_TYPES_H
+#if !defined(__LINUX_SPINLOCK_TYPES_H) && !defined(_ASM_RISCV_SPINLOCK_H)
 # error "please don't include this file directly"
 #endif
 
-typedef struct {
-	volatile unsigned int lock;
-} arch_spinlock_t;
-
-#define __ARCH_SPIN_LOCK_UNLOCKED	{ 0 }
-
-typedef struct {
-	volatile unsigned int lock;
-} arch_rwlock_t;
-
-#define __ARCH_RW_LOCK_UNLOCKED		{ 0 }
+#include <asm-generic/qspinlock_types.h>
+#include <asm-generic/qrwlock_types.h>
 
 #endif /* _ASM_RISCV_SPINLOCK_TYPES_H */
